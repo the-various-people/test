ccc4058a1609fffb7bacc8d760362c273abd4615
the-various-people <variouspeople33@gmail.com>
2022-01-05 01:20:21 +0900

    first commit

---
+++ practice.py
@@ -0,0 +1,8 @@
+def open_account():
+    print('새로운 계좌가 생성되었습니다.')
+
+def deposit(balance, money):
+    print('입금이 완료되었습니다. 잔액은 {0}원입니다.'.format(balance + money))
+    return balance + money
+
+balance
